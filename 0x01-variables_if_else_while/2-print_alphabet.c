fd761ed8dbc817004a418d07c1475aeed210f4ce
Angela-1610 <angelamaged2@gmail.com>
2023-07-14 13:37:12 -0700

    the third code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: return 0 and exit the program
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x <= 'a'; x >= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
