451570813a51531d99cb9726b6202c9f75581705
Angela-1610 <angelamaged2@gmail.com>
2023-07-19 14:38:34 -0700

    the last run

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,14 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/**
-	 * leap year when it can divided
-	 * by 100 and 400 or by 4
-	*/
-
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
