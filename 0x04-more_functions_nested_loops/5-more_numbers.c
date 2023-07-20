5d0f83b439fb61158767dd198ce893efec45f54b
Angela-1610 <angelamaged2@gmail.com>
2023-07-20 13:37:47 -0700

    run

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, res;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			res = j;
+			if (j > 9)
+			{
+				 _putchar(1 + '0');
+				 res = j % 10;
+			}
+			_putchar(res + '0');
 		}
 		_putchar('\n');
 	}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,13 @@ void print_diagonal(int n)
 
 	while (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			for (j = 1; j <= i; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\');
+			_putchar(92);
 			_putchar('\n');
 		}
 		break;
