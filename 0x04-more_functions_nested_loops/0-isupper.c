6e14fdd3158540add62b4d8f735b6c28907068c9
Angela-1610 <angelamaged2@gmail.com>
2023-07-20 09:13:01 -0700

    the first task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * _isupper - Check if c is uppercase
  *
  * @c: input for alphabet
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,5 +15,15 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
 
 #endif
