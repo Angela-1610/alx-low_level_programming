1e38175c3b10076ae0cf19baa8c100ce67ab7624
Angela-1610 <angelamaged2@gmail.com>
2023-07-24 09:15:53 -0700

    the first run

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer and
+ *               updates the value it points to to 98
+ *
+ * @n: the value to point to
+ *
+ * Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *ptr = &n;
+
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ *
+ * @a: the first integer
+ * @b: the seconf integer
+ *
+ * Return: Always 0 (Success)
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
