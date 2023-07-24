80dff7c3226a61769f07fa08fe840a6c851d24f6
Angela-1610 <angelamaged2@gmail.com>
2023-07-24 13:09:32 -0700

    run

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,12 +17,10 @@ void print_array(int *a, int n)
 	{
 		if (i != (n - 1))
 		{
-			_putchar(a[i]);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d,", a[i]);
 		}
 		else
-			_putchar(a[i]);
+			printf("%d", a[i]);
 	}
 	 _putchar('\n');
 }
