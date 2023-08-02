51da87cd3e41e3bc7ce63dac4519482d3b10e3c6
Angela-1610 <angelamaged2@gmail.com>
2023-08-02 12:03:33 -0700

    the first run

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: type string
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int x = 0;
+
+	if (s[x] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s+1);
+
+	_putchar(s[x]);
+	s++;
+
+}
