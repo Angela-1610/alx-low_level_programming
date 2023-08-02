ea34a3d713804da49643be9b6a49f8637c265e91
Angela-1610 <angelamaged2@gmail.com>
2023-08-02 13:54:52 -0700

    the last run

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
+ * _print_rev_recursion - Print a string in reverse.
  * @s: type string
  * Return: Always 0.
  */
-
 void _print_rev_recursion(char *s)
 {
-	int x = 0;
-
-	if (s[x] == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	 _putchar(s[x] + _print_rev_recursion(s - 1));
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,16 +6,12 @@
  * @s: type string
  * Return: Always 0.
  */
-
 int _strlen_recursion(char *s)
 {
-	int counter = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		counter = counter + 1;
-		_strlen_recursion(s);
+		return (0);
 	}
-	else
-		return (counter);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
