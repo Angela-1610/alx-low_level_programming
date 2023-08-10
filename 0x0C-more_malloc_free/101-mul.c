dc0084ea99e731e2b173d3baf7e8849938d2066d
Angela-1610 <angelamaged2@gmail.com>
2023-08-10 12:53:57 -0700

    run

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,15 +10,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, mul;
+	int num1, num2, mul;
 
 	if (argc != 3)
 		printf("Error\n");
 	else
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		mul = n1 * n2;
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
 		printf("%d\n", mul);
 	}
 	return (0);
