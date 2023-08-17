a9cb8fceb80a87d94d3292fc9e1ca38b747d7770
Angela-1610 <angelamaged2@gmail.com>
2023-08-17 15:16:58 -0700

    run

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ptr, n));
+		printf("%d", va_arg(ptr, int));
 
 		if (separator != NULL)
 		{
