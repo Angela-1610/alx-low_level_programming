eaa21be9f1500954c9731c1b0dc28f8edc58182c
git name <mail>
2023-08-25 08:34:22 -0700

    run

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		n++;
+		i++;
 	}
 
-	return (n);
+	return (i);
 }
