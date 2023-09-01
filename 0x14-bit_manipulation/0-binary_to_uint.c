310112198ec9febb3daf833181ecf46b68dfba9b
git name <mail>
2023-09-01 13:26:41 -0700

    bit manipulate

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function converts binary number to int
+ * @b: string of zeros and ones
+ *
+ * Return: number change from binary
+ *         or null if fail
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, sum = 0;
+	unsigned int power;
+
+	if (b == NULL)
+		return (NULL);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != 0 || b[i] != 1)
+			return (0);
+
+		else if (b[i] == 1)
+		{
+			power = pow(2, i);
+			sum = sum + power;
+		}
+	}
+
+	return (sum);
+}
