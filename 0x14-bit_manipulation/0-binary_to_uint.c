c7c4ef8bbaed7c11c9cafae63bdbda88f0b4a928
git name <mail>
2023-09-01 14:29:04 -0700

    run

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,8 +12,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, sum = 0;
-	int power;
+	int i, sum = 0;
 
 	if (b == NULL)
 		return (0);
@@ -26,10 +25,11 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
+		sum <<= 1;
+
 		if (b[i] == '1')
 		{
-			power = pow(2, i);
-			sum = sum + power;
+			sum = sum + 1;
 		}
 	}
 
