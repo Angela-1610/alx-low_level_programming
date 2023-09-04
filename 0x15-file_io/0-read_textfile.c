66e8ee1dd7c02c6464e521ec3032d8fac9339ce5
git name <mail>
2023-09-04 11:44:10 -0700

    read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - function reads text file and prints it
+ * @filename: pointer of file
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *arr;
+
+	arr = malloc(sizeof(char) * letters);
+
+	if (arr == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, arr, letters);
+	wr = write(STDOUT_FILENO, arr, rd);
+
+	if (op == -1 | rd == -1 | wr == -1)
+		return (0);
+
+	close(o);
+	free(arr);
+
+	return (wr);
+}
