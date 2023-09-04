ff81c668aae6ec727456a2a74a1e2f6a8d8d6bec
git name <mail>
2023-09-04 13:25:21 -0700

    file task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: file name to creat it
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+	}
+
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	wr = write(op, text_content, i);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include"main.h"
+
+/**
+ * append_text_to_file - function appends text at end of file
+ * @filename: file name to creat it
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, i);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
+
