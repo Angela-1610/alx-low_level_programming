bb22fd164ed17f9ddfdb708238a6fb20718508e8
git name <mail>
2023-09-04 13:34:17 -0700

    extra run

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i]; i++)
 			;
 	}
 
-	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	op = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
 	wr = write(op, text_content, i);
 
 	if (op == -1 || wr == -1)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i]; i++)
 			;
 	}
 
