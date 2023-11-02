131fc09c51278a342a2c1283e5d6096520b20007
Angela-1610 <angelamaged2@gmail.com>
2023-11-02 14:40:37 -0700

    done

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hash table
+ * @size: var of type int
+ *
+ * Return: new table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *t;
+	unsigned long int i;
+
+	t = malloc(sizeof(hash_table_t));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	t->size = size;
+	t->array = malloc(sizeof(hash_node_t *) * size);
+	if (!t->array)
+	{
+		free(t);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		t->array[i] = NULL;
+	}
+
+	return (t);
+}
