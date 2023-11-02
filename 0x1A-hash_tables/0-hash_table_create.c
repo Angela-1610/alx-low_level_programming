d6107b6a4deb003576240918e3882f61ec1ade95
Angela-1610 <angelamaged2@gmail.com>
2023-11-02 15:03:16 -0700

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,9 +4,9 @@
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: the hash table
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
